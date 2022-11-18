4f54e3d0c56e6a26fb16de545113cedb64d20b38
SaeedQatanani <saeedqatananil@gmail.com>
2022-11-18 18:50:35 +0200

    initial commit

---
+++ worldcupspothub/src/main/java/com/axsos/worldcupspothub/WorldcupspothubApplication.java
@@ -0,0 +1,13 @@
+package com.axsos.worldcupspothub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldcupspothubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldcupspothubApplication.class, args);
+	}
+
+}
---
+++ worldcupspothub/src/test/java/com/axsos/worldcupspothub/WorldcupspothubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axsos.worldcupspothub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldcupspothubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
